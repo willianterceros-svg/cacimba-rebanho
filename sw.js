795b8aac943925966409f69fde125ae2c41f7698
willianterceros-svg <willianterceros@gmail.com>
2026-08-27 07:17:43 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cacimba-rebanho-v20260824-cloud-v1";
+const CACHE_NAME = "cacimba-rebanho-v20260827-pedigree-v1";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
